341556725f6edc7301196d56e7523274eed7b00b
Rylee Hilde <ryleehilde@u.boisestate.edu>
2025-11-19 15:49:39 -0700

    Added equations for finding next state in binary; HAS NOT BEEN TESTED

--- binary.v
+++ binary.v
@@ -9,25 +9,30 @@ module binary(
     wire [2:0] Next;
 
     dff zero(
+        .reset(reset),
         .D(Next[0]),
         .clk(clk),
         .Q(State[0])
     );
 
     dff one(
+        .reset(reset),
         .D(Next[1]),
         .clk(clk),
         .Q(State[1])
     );
     
     dff two(
+        .reset(reset),
         .D(Next[2]),
         .clk(clk),
         .Q(State[2])
     );
     
     assign z = State[1] & ~State[0];
-    assign Next[0] = w & ~State[1] & ~State[0];
-    assign Next[1] = w & (State[1] | State[0]);
+    assign Next[0] = (~State[2] & ~State[1] & ~State[0]) | (~State[2] & ~State[0] & w) | (~State[2] & ~State[1] & w) | (State[2] & ~State[1] & ~State[0] & ~w) | (~State[2] & State[1] & State[0] & ~w);
+    assign Next[1] = (~State[1] & State[0]) | (State[1] & ~State[0]) | (~State[2] & ~State[1] & w);
+    assign Next[2] = (State[2] & State[0]) | (State[1] & State[0] & w);
+
 
 endmodule
