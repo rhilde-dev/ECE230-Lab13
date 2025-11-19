5b9a03b6ac2c400efd16fb468ad6d43f6c234fd2
Rylee Hilde <ryleehilde@u.boisestate.edu>
2025-11-19 15:11:45 -0700

    Implemented onehot module and started binary module

--- binary.v
+++ binary.v
@@ -1,1 +1,33 @@
-// Implement binary state machine
+module binary(
+    input w,
+    input clk,
+    input reset,
+    output z
+);
+
+    wire [2:0] State;
+    wire [2:0] Next;
+
+    dff zero(
+        .D(Next[0]),
+        .clk(clk),
+        .Q(State[0])
+    );
+
+    dff one(
+        .D(Next[1]),
+        .clk(clk),
+        .Q(State[1])
+    );
+    
+    dff two(
+        .D(Next[2]),
+        .clk(clk),
+        .Q(State[2])
+    );
+    
+    assign z = State[1] & ~State[0];
+    assign Next[0] = w & ~State[1] & ~State[0];
+    assign Next[1] = w & (State[1] | State[0]);
+
+endmodule
--- onehot.v
+++ onehot.v
@@ -1,1 +1,57 @@
-// Implement one-hot state machine
+module one_hot(
+    input w,
+    input clk,
+    input reset,
+    output z
+);
+    wire Anext, Bnext, Cnext, Dnext, Enext;
+    wire Astate, Bstate, Cstate, Dstate, Estate;
+
+    dff Adff(
+        .Default(1'b1),
+        .D(Anext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Astate)
+    );
+
+    dff Bdff(
+        .Default(1'b0),
+        .D(Bnext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Bstate)
+    );
+
+    dff Cdff(
+        .Default(1'b0),
+        .D(Cnext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Cstate)
+    );
+    
+    dff Ddff(
+        .Default(1'b0),
+        .D(Dnext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Dstate)
+    );
+    
+    dff Edff(
+        .Default(1'b0),
+        .D(Enext),
+        .clk(clk),
+        .reset(reset),
+        .Q(Estate)
+    );
+
+    assign z = Cstate;
+
+    assign Anext = 1'b0;
+    assign Bnext = ~w & (Astate | Dstate | Estate);
+    assign Cnext = ~w & (Bstate | Cstate);
+    assign Dnext = w & (Astate | Bstate | Cstate);
+    assign Enext = w & (Dstate | Estate);
+endmodule
